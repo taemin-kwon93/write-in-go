370b518e5fba20e8d9f498dc2159eadc02787527
taemin-kwon93 <4146kt@gmail.com>
2023-04-19 23:30:46 +0900

    study

--- 003-print-something/print_some.go
+++ 003-print-something/print_some.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	strAndQuotedStr()
+	// strAndQuotedStr()
+	scanSomeWord()
 }
 
 func strAndQuotedStr() {
 	str := "Hello, World\nIt Good To See U!"
 	fmt.Printf("%s", str)
 	fmt.Printf("%q", str) // quoted string literal
+}
+
+func scanSomeWord() {
+	var word string
+	fmt.Scan(&word)
+	fmt.Println(word)
 }
